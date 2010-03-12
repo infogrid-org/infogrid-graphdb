1fb4844222a8885bc63e725a087b411a358f9d6a
Johannes Ernst <Johannes.Ernst@InfoGrid.org>
2010-03-12 19:44:44 +0000

    sanitized formatting of Identifiers; it's still not totally sane but a lot more so some cleanup around Stringification

--- ig-graphdb/modules/org.infogrid.kernel/src/org/infogrid/model/primitives/text/JavadocHtmlStringStringifier.java
+++ ig-graphdb/modules/org.infogrid.kernel/src/org/infogrid/model/primitives/text/JavadocHtmlStringStringifier.java
@@ -8,58 +8,72 @@
 //
 // For more information about InfoGrid go to http://infogrid.org/
 //
-// Copyright 1998-2009 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
+// Copyright 1998-2010 by R-Objects Inc. dba NetMesh Inc., Johannes Ernst
 // All rights reserved.
 //
 
 package org.infogrid.model.primitives.text;
 
-import org.infogrid.util.text.HtmlStringStringifier;
-import org.infogrid.util.text.StringRepresentationParameters;
+import org.infogrid.util.text.AbstractDelegatingStringifier;
+import org.infogrid.util.text.Stringifier;
 
 /**
- * A HtmlStringStringifier that escapes star-slash (the Java end-of-comment indicator)
+ * A HtmlifyingDelegatingStringifier that escapes star-slash (the Java end-of-comment indicator)
  * so emitted Html is safe to be used in Javadoc.
+ *
+ * @param <T> the type of the Objects to be stringified
  */
-public class JavadocHtmlStringStringifier
+public class JavadocHtmlStringStringifier<T>
         extends
-            HtmlStringStringifier
+            AbstractDelegatingStringifier<T>
 {
     /**
      * Factory method.
      *
+     * @param delegate the underlying Stringifier that knows how to deal with the real type
      * @return the created JavadocHtmlStringStringifier
+     * @param <T> the type of the Objects to be stringified
      */
-    public static JavadocHtmlStringStringifier create()
+    public static <T> JavadocHtmlStringStringifier<T> create(
+            Stringifier<T> delegate )
     {
-        return new JavadocHtmlStringStringifier();
+        return new JavadocHtmlStringStringifier<T>( delegate );
     }
 
     /**
-     * No-op constructor. Use factory method.
-     */
-    protected JavadocHtmlStringStringifier()
-    {
-        // no op
-    }
-
-    /**
-     * Format an Object using this Stringifier.
+     * Constructor. Use factory method.
      *
-     * @param soFar the String so far, if any
-     * @param arg the Object to format, or null
-     * @param pars collects parameters that may influence the String representation
-     * @return the formatted String
+     * @param delegate the underlying Stringifier that knows how to deal with the real type
      */
-    @Override
-    public String format(
-            String                         soFar,
-            String                         arg,
-            StringRepresentationParameters pars )
+    protected JavadocHtmlStringStringifier(
+            Stringifier<T> delegate )
     {
-        String raw = super.format( soFar, arg, pars );
+        super( delegate );
+    }
 
-        String ret = raw.replaceAll( "\\*/", "&#42;/" );
+    /**
+     * Overridable method to possibly escape a String first.
+     *
+     * @param s the String to be escaped
+     * @return the escaped String
+     */
+    protected String escape(
+            String s )
+    {
+        String ret = s.replaceAll( "\\*/", "&#42;/" );
+        return ret;
+    }
+
+    /**
+     * Overridable method to possibly unescape a String first.
+     *
+     * @param s the String to be unescaped
+     * @return the unescaped String
+     */
+    protected String unescape(
+            String s )
+    {
+        String ret = s.replaceAll( "&#42;/", "\\*/" );
         return ret;
     }
 }
